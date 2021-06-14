e4e9526e564402ad4795691a1e4001c1d261a9d9
Jason Chang <jason.chang@sizztech.com>
2021-06-14 20:16:25 +1000

    initial commit - legacy project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace RuneImageCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rune image collector initiated!");
+            while (true)
+            {
+                var path = Directory.GetCurrentDirectory();                
+                var runeImage = Directory.GetFiles(path, "2.png", SearchOption.TopDirectoryOnly);
+                if (runeImage.Length > 0)
+                {
+                    int count = Directory.GetFiles(path, "*", SearchOption.TopDirectoryOnly).Length;                    
+                    File.Move("2.png", "b_" + count + ".png");
+                    Console.WriteLine($"New rune image found, saved as {count}.png");
+                }                
+                Thread.Sleep(5000);
+            }           
+        }
+    }
+}
